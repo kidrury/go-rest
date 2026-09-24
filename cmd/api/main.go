83472ac5c012176d3f94747a7bec7d3b0d599066
thecodekid <alirezaahmadi260979@gmail.com>
2026-09-24 12:17:28 +0330

    chore: remove commented lines

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -17,66 +17,12 @@ import (
 )
 
 func main() {
-	// if err := godotenv.Load(); err != nil {
-	// 	slog.Warn("could not load .env file", "error", err)
-	// }
 	godotenv.Load()
 	conf, err := config.Load()
 	if err != nil {
 		slog.Error("failed loading configurations", "error", err)
 		os.Exit(1)
 	}
-
-	// tm, err := auth.NewTokenManager(conf.JWTSecret, conf.JWTIssuer, conf.JWTAudience, conf.AccessTokenTTL)
-
-	// if err != nil {
-	// 	slog.Error("failed creating token manager", "error", err)
-	// 	os.Exit(1)
-	// }
-
-	// pool, err := database.NewPostgresPool(context.Background(), conf.DatabaseURL)
-	// if err != nil {
-	// 	slog.Error("failed creating postgres pool", "error", err)
-	// 	os.Exit(1)
-	// }
-
-	// userRepo := postgres.NewUserRepository(pool)
-
-	// sessionRepo := postgres.NewSessionRepository(pool)
-
-	// authService, err := service.NewAuthService(userRepo, sessionRepo, tm, conf.RefreshTokenTTL, conf.RefreshIdleTTL)
-	// if err != nil {
-	// 	slog.Error("failed creating auth service", "error", err)
-	// 	os.Exit(1)
-	// }
-
-	// authHandler := handler.NewAuthHandler(authService, conf.AccessTokenTTL)
-
-	// userService := service.NewUserService(userRepo)
-	// userHandler := handler.NewUserHandler(userService)
-
-	// mux := http.NewServeMux()
-
-	// handler := middleware.Recover(mux)
-	// handler = middleware.Log(handler)
-	// handler = middleware.RequestID(handler)
-
-	// mux.HandleFunc("GET /health/live", h.Live)
-	// mux.HandleFunc("GET /health/ready", h.Ready)
-	// mux.HandleFunc("GET /health/notexist", h.NotExistTest)
-	// mux.HandleFunc("GET /health/created", h.CreatedTest)
-	// mux.HandleFunc("GET /health/panic", h.PanicTest)
-	// mux.HandleFunc("GET /health/panic-after-write", h.PanicAfterWriteTest)
-	// mux.Handle("POST /user", h.HandlerFunc(h.CreateUser))
-	// mux.Handle("POST /validate", h.HandlerFunc(h.TestValidation))
-	// mux.Handle("GET /user/{id}", middleware.Auth(tm)(h.HandlerFunc(userHandler.GetUser)))
-	// mux.Handle(
-	// 	"GET /user",
-	// 	middleware.Auth(tm)(middleware.RequirePermission(userService, authz.PermissionReadAny)(h.HandlerFunc(h.ListUsers))),
-	// )
-	// mux.Handle("POST /auth/login", h.HandlerFunc(authHandler.Login))
-	// mux.Handle("POST /auth/refresh", middleware.Auth(tm)(h.HandlerFunc(authHandler.Refresh)))
-	// mux.Handle("POST /auth/logout", middleware.Auth(tm)(h.HandlerFunc(authHandler.Logout)))
 
 	application, err := app.New(*conf)
 	if err != nil {
